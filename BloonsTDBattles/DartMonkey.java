e5d6879220c837a1a713e948f1dc980a7e0f7d51
David <divadzirk2004@gmail.com>
2020-01-07 11:49:52 -0500

    changed dart monkey attack delay

--- BloonsTDBattles/DartMonkey.java
+++ BloonsTDBattles/DartMonkey.java
@@ -38,6 +38,6 @@ public class DartMonkey extends Tower
         Dart dart = new Dart();
         getWorld().addObject(dart, getX(), getY());
         dart.setRotation(getRotation() + 90);
-        attackDelay = 30;
+        attackDelay = 50;
     }
 }
