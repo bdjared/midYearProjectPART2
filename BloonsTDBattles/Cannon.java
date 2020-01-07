82fe6ac78719332893233a4f15fbd1c3b9afdaa5
bdjared <jarhyn22@bergen.org>
2020-01-07 10:54:55 -0500

    Added cannon

--- BloonsTDBattles/Cannon.java
+++ BloonsTDBattles/Cannon.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.*;
 /**
  * Write a description of class Cannon here.
  * 
@@ -8,12 +8,40 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Cannon extends Tower
 {
+    public int range = 13;
+    public double attackDelay = 100;
+    private List<Balloon> bloonsInRange;
+    public Projectile projectileType;
+    private Cannonball cannonball;
+    
+    
+    public Cannon() {
+        getImage().scale(1500,1500);
+    }
+       
     /**
      * Act - do whatever the Cannon wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
+        bloonsInRange = super.findBalloons(range);
+        Collections.sort(bloonsInRange); 
+        if (!bloonsInRange.isEmpty()) {
+            attack();
+            aim();
+            
+        }
     }    
+    public void aim() {
+        Balloon target = bloonsInRange.get(0);
+        turnTowards(target.getX(), target.getY());
+        turn(90);
+    }
+    public void attack() {
+        if (cannonball == null) {
+            cannonball = new Cannonball(getRotation() + 90, this);
+    }
+    cannonball.act();
+}
 }
