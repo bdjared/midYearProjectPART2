27a89b2966310ada2f7dc81370f1ea2febdcb3d9
bdjared <jarhyn22@bergen.org>
2020-01-09 11:42:12 -0500

    Added to the my World class

--- BloonsTDBattles/MyWorld.java
+++ BloonsTDBattles/MyWorld.java
@@ -8,7 +8,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-    int i = 0;
+    public int guapo = 250;
+    public boolean finito = false;
+    private int health;
+    private int roundsCompleted = 0;
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -20,5 +23,44 @@ public class MyWorld extends World
     }
     
     public void act(){
+        
+    }
+    
+    public void Round1 () {
+        for (int i = 0; i<=15; i++) {
+           addObject(new redBalloon(), 0, 0);
+        }
+      /*  if (getObjects(Balloon.class)) {
+        finito = true;
+    } */
+        if (finito = true) {
+            guapo += 100;
+            showText("Round finished! Press Spacebar to continue to the text round", 270, 200);
+            if (Greenfoot.isKeyDown("SPACEBAR")){
+                Round2();
+            }
+        }
+    }
+    
+    public void Round2() {
+        for (int i = 0; i<=25; i++) {
+            addObject(new redBalloon(), 0, 0);
+        }
+    finito = true; 
+    if (finito = true) {
+        showText("Round finished! Press Spacebar to continue to the text round", 270, 200);
+        if (Greenfoot.isKeyDown("SPACEBAR")){
+             Round3();
+          }
+        }
+    }
+    public void Round3() {
+        for (int i = 0; i<=10; i++) {
+            addObject(new blueBalloon(), 0, 0);
+            for (int g = 0; g<1; g++) {
+             addObject(new redBalloon(), 0, 0);
+        }
     }
 }
+}
+
